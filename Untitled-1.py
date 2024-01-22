2bf9bd76644342b94ef21c7e9bdc0cdfc3e72489
kadir-zeren <k4dzeren@gmail.com>
2024-01-22 10:56:27 +0300

    assign

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,30 @@
 print("hello world")
+# bu bir yorum
+# bu ikinci yorum
+# bu üçüncü yorum
+
+a="bu benim yazdığım satır\
+    stringe devam ediyorum"
+
+print(a)
+
+
+city = "istanbul"
+print(city)
+
+sayi = 35
+print(sayi)
+
+
+a = 1
+b = 2
+c = 3
+print(a,b,c)
+
+
+my_age = 33
+your_age = 30
+my_age = your_age
+print(my_age)
+
+help("keywords")
