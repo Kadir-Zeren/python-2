4712951a5a2f617c8141afaf4e08cecd30342190
kadir-zeren <k4dzeren@gmail.com>
2024-01-22 17:27:38 +0300

    type

--- Untitled-1.py
+++ Untitled-1.py
@@ -28,3 +28,70 @@ my_age = your_age
 print(my_age)
 
 help("keywords")
+
+x = y = z = "same" 
+print(x, y,x)
+
+
+jan = marc = may = july = sep= nov=31
+fab = apr= jun = sep = nov=30
+dec =29
+print(jan,fab,marc,apr)
+
+walter = None
+print(walter)
+
+
+a, b ,c = 5, 6, 7
+print(a,b,c)
+
+
+a = "istanbul"
+print(type(a))
+
+sehir = "35"
+print(type(sehir),type(walter))
+
+sayi1 = 40
+print(type(sayi1))
+
+a = 6.7
+print(type(a))
+
+b = True
+
+print(type(a), type(b))
+
+num = 5
+print(type(num))
+num = str(num)
+print(type(num))
+
+pi = 3.14
+converted_pi = str(pi)
+print(converted_pi)
+print(type(converted_pi)) 
+
+a=1.23
+print(
+type(a)
+
+)
+a = int(a)
+print(a)
+print(type(a))
+
+b = 7
+
+print(type(b))
+b = float(b)
+print(type(b))
+
+x = 39 
+v = "11"
+y = "2.5"
+z = "I am at_"
+
+print(x-int(v))
+print(x-float(y))
+print(z+str(x))
